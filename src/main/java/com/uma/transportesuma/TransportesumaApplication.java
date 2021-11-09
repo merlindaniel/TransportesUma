b3e95e7c292a0483aa6c693ef97c61f05fda95fa
merlindaniel <merlindaniel30@gmail.com>
2021-11-09 20:30:01 +0100

    primer commit

---
+++ src/main/java/com/uma/transportesuma/TransportesumaApplication.java
@@ -0,0 +1,13 @@
+package com.uma.transportesuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportesumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportesumaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uma/transportesuma/TransportesumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uma.transportesuma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportesumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
