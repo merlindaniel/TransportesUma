857e68b33fe7c3ac816d5a400fd15f36dfbefad9
merlindaniel <merlindaniel30@gmail.com>
2022-02-15 18:25:01 +0100

    Google OAuth Added

--- src/main/java/com/uma/transportesuma/security/UserAuthentication.java
+++ src/main/java/com/uma/transportesuma/security/UserAuthentication.java
@@ -3,10 +3,21 @@ package com.uma.transportesuma.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.uma.transportesuma.document.User;
+import com.uma.transportesuma.dto.UserDTO;
+import com.uma.transportesuma.service.UserService;
 import com.uma.transportesuma.vo.JWTToken;
 import com.uma.transportesuma.vo.UserAuth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,8 +27,9 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.*;
 
 @RestController
 @RequestMapping("/authentication/user")
@@ -29,13 +41,21 @@ public class UserAuthentication {
     @Value("${my.jwt.privatekey}")
     private String SECRET_KEY;
 
+    private static final String GOOGLE_PUBLIC_CLIENT_ID = "634135800903-t4lnnd9m66u3lr82d4h4ncat75qbt1br.apps.googleusercontent.com";
+    private static final String GOOGLE_CLIENT_SECRET = "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";
 
     AuthenticationManager authenticationManager;
+    private UserService userService;
 
     @Autowired
     public void setAuthenticationManager(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
 
 
     //Logea y CREA un token JWT
@@ -53,9 +73,57 @@ public class UserAuthentication {
     }
 
     @PostMapping("/google/oauth")
-    public JWTToken authenticateGoogleOAuth(){
-        return null;
+    public ResponseEntity<JWTToken> authenticateGoogleOAuth(@RequestBody String postBody) {
+        try{
+            JsonObject item = JsonParser.parseString(postBody).getAsJsonObject();
+            String id_token = item.get("id_token").getAsString();
 
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                    .setAudience(Collections.singletonList(GOOGLE_PUBLIC_CLIENT_ID))
+                    .build();
+
+            GoogleIdToken idToken = verifier.verify(id_token);
+            if (idToken != null) {
+                GoogleIdToken.Payload payload = idToken.getPayload();
+
+                // Print user identifier
+                String userId = payload.getSubject();
+                System.out.println("User ID: " + userId);
+
+                // Get profile information from payload
+                String email = payload.getEmail();
+                String name = (String) payload.get("name");
+                String pictureUrl = (String) payload.get("picture");
+                String givenName = (String) payload.get("given_name");
+                String randomPassword = getRandomString();
+
+                Optional<User> thisUser = this.userService.findUserByEmail(email);
+                User u = null;
+                if(!thisUser.isPresent()) {
+                    UserDTO userDTO = new UserDTO(email, email, name, randomPassword, pictureUrl, "");
+                    u = this.userService.addUser(userDTO);
+                } else {
+                    u = thisUser.get();
+                    u.setPassword(randomPassword);
+                    u = this.userService.updateUser(u);
+                }
+
+                Authentication authentication = authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(u.getUsername(), randomPassword)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                String tokenJwt = this.getTokenJWT(u.getUsername());
+
+                return new ResponseEntity<>(new JWTToken(tokenJwt), HttpStatus.OK);
+            } else {
+                throw new Exception("Invalid ID token.");
+            }
+
+        } catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
 
@@ -77,5 +145,18 @@ public class UserAuthentication {
 
         }
         return null;
+    }
+
+    private String getRandomString(){
+        int leftLimit = 97;
+        int rightLimit = 122;
+        int targetStringLength = 20;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        return generatedString;
     }
 }
